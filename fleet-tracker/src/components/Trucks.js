9b5d5c9397e5a6645f622593bcfe65cee2790c53
Justin Huffman <justin.t.huffman@gmail.com>
2018-11-28 11:27:55 -0500

    configured react, started truck view

---
+++ fleet-tracker/src/components/Trucks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Trucks extends Component {
+
+
+
+
+    render() {
+        return (
+        	<div>
+        		<h1>Trucks</h1>
+        	</div>
+        );
+    }
+}
+
+export default Trucks;
