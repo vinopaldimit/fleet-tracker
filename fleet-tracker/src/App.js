8c50b242b6dba7dce853006cfb016d1bd9dbac01
Justin Huffman <justin.t.huffman@gmail.com>
2018-11-28 16:06:03 -0500

    Created truck and trip views

--- fleet-tracker/src/App.js
+++ fleet-tracker/src/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 import Trucks from './components/Trucks';
+import Trips from './components/Trips';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            trucks: []
+            trucks: [],
+            trips: [],
+            currentView: ''
+
         }
     }
     getTrucks() {
         fetch('http://localhost:8080/api/trucks').then(res => res.json())
             .then(data => {
                 console.log(data);
-                this.setState({trucks:data})
+                this.setState({ trucks: data })
             })
     }
-	componentDidMount(){
-		this.getTrucks()
-	}
+
+    getTrips() {
+        fetch('http://localhost:8080/api/trips').then(res => res.json())
+            .then(data => {
+                console.log(data);
+                this.setState({ trips: data })
+            })
+    }
+
+
+    componentDidMount() {
+        this.getTrucks()
+        this.getTrips()
+    }
+    setTrucks=()=> {
+        this.setState({ currentView: 'trucks' })
+    }
+    setTrips=()=> {
+        this.setState({ currentView: 'trips' })
+    }
     render() {
 
         return (
@@ -26,9 +47,25 @@ class App extends Component {
         <header>
         </header>
         <main>
-        	<button className="trucks">Trucks</button>
-        	<button className="trips">Trips</button>
-        	<Trucks trucks={this.state.trucks} />
+        	<button className="trucks" onClick={this.setTrucks}>Trucks</button>
+        	<button className="trips" onClick={this.setTrips}>Trips</button>
+        	
+
+        
+			
+        <div>
+          {this.state.currentView === 'trucks'
+          ? <Trucks trucks={this.state.trucks} />
+          : this.state.currentView === 'trips'
+		  ? <Trips trips={this.state.trips} />
+          : <h2>Choose your destiny</h2>}
+        
+        </div>
+
+
+
+
+
         </main>
       </div>
         );
--- fleet-tracker/src/components/Trips.js
+++ fleet-tracker/src/components/Trips.js
@@ -1,19 +1,41 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
-class Component extends Component {
-    static propTypes = {
-        className: PropTypes.string,
-    };
+class Trips extends Component {
 
-    constructor(props) {
-        super(props);
-    }
+constructor (props) {
+    super(props);
+}
+
 
     render() {
-        return (
-            
-        );
+        return (<div>
+                {this.props.trips.map((trip, index) =>
+                    <section key={index}>
+                        <h2>{trip.tripNumber}</h2>
+                        <p>{trip.date}</p>
+                        <p>{trip.origin}</p>
+                        <p>{trip.destination}</p>
+                        <p>{trip.zipCode}</p>
+                        <p>{trip.milesQuantity}</p>
+                        <p>{trip.vMr}</p>
+                        <p>{trip.mileagePlus}</p>
+                        <p>{trip.premiums}</p>
+                        <p>{trip.fuel}</p>
+                        <p>{trip.totalRate}</p>
+                        <p>{trip.aMt}</p>
+                        <p>{trip.packages}</p>
+                        <p>{trip.packageAmt}</p>
+                        <p>{trip.dropAndHook}</p>
+                        <p>{trip.tolls}</p>
+                        <p>{trip.flatRate}</p>
+                        <p>{trip.dailyGrossAmount}</p>
+                        <p>{trip.driverOne}</p>
+                        <p>{trip.driverTwo}</p>
+
+                    </section>
+                )}
+            </div>);
     }
 }
 
-export default Component;
+export default Trips;
