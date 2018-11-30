4e0b27ae6b3a394333a0dd8ff77dd943eb72e296
KevinFehrenbach17 <x2g1cx@gmail.com>
2018-11-30 10:02:52 -0500

    i did some stuff look at it

--- src/main/java/org/wecancodeit/fleettracker/controller/ApiController.java
+++ src/main/java/org/wecancodeit/fleettracker/controller/ApiController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wecancodeit.fleettracker.models.Company;
 import org.wecancodeit.fleettracker.models.Employee;
+import org.wecancodeit.fleettracker.models.FedExAssignment;
 import org.wecancodeit.fleettracker.models.Trip;
 import org.wecancodeit.fleettracker.models.Truck;
 import org.wecancodeit.fleettracker.repositories.CompanyRepository;
 import org.wecancodeit.fleettracker.repositories.EmployeeRepository;
+import org.wecancodeit.fleettracker.repositories.FedExAssignmentRepository;
 import org.wecancodeit.fleettracker.repositories.TripRepository;
 import org.wecancodeit.fleettracker.repositories.TruckRepository;
 
@@ -25,12 +27,15 @@ public class ApiController {
 
 	@Autowired
 	private TripRepository tripRepo;
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepo;
-	
+
 	@Autowired
 	private CompanyRepository companyRepo;
+
+	@Autowired
+	private FedExAssignmentRepository assignmentRepo;
 
 	@GetMapping("/trucks")
 	public Iterable<Truck> getTrucks() {
@@ -52,7 +57,7 @@ public class ApiController {
 		return tripRepo.findById(id).get();
 
 	}
-	
+
 	@GetMapping("/employees")
 	public Iterable<Employee> getEmployees() {
 		return employeeRepo.findAll();
@@ -63,7 +68,7 @@ public class ApiController {
 		return employeeRepo.findById(id).get();
 
 	}
-	
+
 	@GetMapping("/companies")
 	public Iterable<Company> getCompanies() {
 		return companyRepo.findAll();
@@ -72,6 +77,17 @@ public class ApiController {
 	@GetMapping("/companies/{id}")
 	public Company getCompany(@PathVariable(value = "id") Long id) {
 		return companyRepo.findById(id).get();
+
+	}
+
+	@GetMapping("/companies")
+	public Iterable<FedExAssignment> getCompanies() {
+		return FedExAssignmentRepo.findAll();
+	}
+
+	@GetMapping("/companies/{id}")
+	public FedExAssignment getFedExAssignment(@PathVariable(value = "id") Long id) {
+		return FedExAssignmentRepo.findById(id).get();
 
 	}
 
