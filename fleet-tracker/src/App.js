edd085dab805c7b3c9b4cc77d75d35fa86cf7828
vinopaldimit <43007053+vinopaldimit@users.noreply.github.com>
2018-12-06 11:45:38 -0500

    built out react calculations

--- fleet-tracker/src/App.js
+++ fleet-tracker/src/App.js
@@ -5,6 +5,7 @@ import Employees from './components/Employees';
 import FuelPurchases from './components/FuelPurchases';
 import FedExAssignments from './components/FedExAssignments';
 import Companies from './components/Companies';
+import Calculations from './components/Calculations';
 
 class App extends Component {
     constructor() {
@@ -20,6 +21,7 @@ class App extends Component {
 
         }
     }
+
     getCompanies() {
         fetch('http://localhost:8080/api/companies').then(res => res.json())
             .then(data => {
@@ -27,6 +29,7 @@ class App extends Component {
                 this.setState({ companies: data })
             })
     }
+
     getFedExAssignments() {
         fetch('http://localhost:8080/api/fedexassignments').then(res => res.json())
             .then(data => {
@@ -51,6 +54,7 @@ class App extends Component {
                 this.setState({ employees: data })
             })
     }
+
     getTrucks() {
         fetch('http://localhost:8080/api/trucks').then(res => res.json())
             .then(data => {
@@ -67,8 +71,7 @@ class App extends Component {
             })
     }
 
-
-    componentDidMount() {
+    fetchAll() {
         this.getTrucks()
         this.getTrips()
         this.getEmployees()
@@ -76,23 +79,111 @@ class App extends Component {
         this.getFedExAssignments()
         this.getCompanies()
     }
+
+    componentWillMount() {
+        this.fetchAll()
+    }
+
+    countTotalMiles = () => {
+        let miles = 0
+        this.state.trips.map(function(trip, index){
+            miles += trip.milesQuantity
+        })
+        return(miles)
+    }
+
+    countTotalAMT = () => {
+        let AMT = 0
+        this.state.trips.map(function(trip, index){
+            AMT += trip.aMt
+        })
+        return(AMT)
+    }
+
+    countTotalPackages = () => {
+        let packages = 0
+        this.state.trips.map(function(trip, index){
+            packages += trip.packages
+        })
+        return(packages)
+    }
+
+    countTotalPackageAmt = () => {
+        let packageAmt = 0
+        this.state.trips.map(function(trip, index){
+            packageAmt += trip.packageAmt
+        })
+        return(packageAmt)
+    }
+
+    countTotalDropAndHook = () => {
+        let dropAndHook = 0
+        this.state.trips.map(function(trip, index){
+            dropAndHook += trip.dropAndHook
+        })
+        return(dropAndHook)
+    }
+
+    countTotalTolls = () => {
+        let tolls = 0
+        this.state.trips.map(function(trip, index){
+            tolls += trip.tolls
+        })
+        return(tolls)
+    }
+
+    countTotalFlatRate = () => {
+        let flatRate = 0
+        this.state.trips.map(function(trip, index){
+            flatRate += trip.flatRate
+        })
+        return(flatRate)
+    }
+
+    countTotalDailyGrossAmount = () => {
+        let dailyGross = 0
+        this.state.trips.map(function(trip, index){
+            dailyGross += trip.dailyGrossAmount
+        })
+        return(dailyGross)
+    }
+
+    countUniqueTrucks = () => {
+        let truckList = []
+        this.state.trips.map(function(trip, index){
+            if(!truckList.includes(trip.truck.id)){
+                truckList.push(trip.truck.id)
+            }
+        })
+        return(truckList.length)
+    }
+
     setTrucks = () => {
         this.setState({ currentView: 'trucks' })
     }
+
     setTrips = () => {
         this.setState({ currentView: 'trips' })
     }
+
     setEmployees = () => {
         this.setState({ currentView: 'employees' })
     }
+
     setFuelPurchases = () => {
         this.setState({ currentView: 'fuelPurchases' })
     }
+
     setFedExAssignments = () => {
         this.setState({ currentView: 'fedExAssignments' })
     }
+
     setCompanies = () => {
         this.setState({ currentView: 'companies' })
+    }
+
+    setCalculations = ( ) => {
+        this.setState({ currentView: 'calculations' })
     }
 
     render() {
@@ -108,6 +199,7 @@ class App extends Component {
                         <button className="fuelPurchases" onClick={this.setFuelPurchases}>Fuel Purchases</button>
                         <button className="fedExAssignments" onClick={this.setFedExAssignments}>FedEx Assignments</button>
                         <button className="companies" onClick={this.setCompanies}>Companies</button>
+                        <button className="calculations" onClick={this.setCalculations}>Calculations</button>
                     </nav>
                 </header>
                 <main>
@@ -124,6 +216,16 @@ class App extends Component {
                       ? <FedExAssignments fedExAssignments={this.state.fedExAssignments} />
                       : this.state.currentView === 'companies'
                       ? <Companies companies={this.state.companies} />
+                      : this.state.currentView === 'calculations'
+                      ? <Calculations countTotalMiles={this.countTotalMiles} 
+                        countTotalAMT={this.countTotalAMT} 
+                        countTotalPackages={this.countTotalPackages} 
+                        countTotalPackageAmt={this.countTotalPackageAmt}
+                        countTotalDropAndHook={this.countTotalDropAndHook}
+                        countTotalTolls={this.countTotalTolls}
+                        countTotalFlatRate={this.countTotalFlatRate}
+                        countTotalDailyGrossAmount={this.countTotalDailyGrossAmount}
+                        countUniqueTrucks={this.countUniqueTrucks}/>
                       : <h2></h2>}
                     
                     </div>
